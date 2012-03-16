6b97e7539962955ff0309e573bea50fee05246f6
Jude Hungerford <edward.h@gmx.net>
2012-03-17 02:39:33 +1100

    update example-game.sh and README.org

--- wargame/example-game.sh
+++ wargame/example-game.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-
-./playgame.py --verbose --fill --log_input --log_output --log_error --nolaunch --log_dir game_logs --turns 500 --map_file maps/test/test_p04_02.map "python dist/sample_bots/python/LeftyBot.py" "python dist/sample_bots/python/RandomBot.py"
+python2 playgame.py -v --nolaunch -l game_logs -I -O -E --turns 100 --map_file maps/test/war_p02_01.map \
+"absolute-path-to/aichallenge/wargame/dist/starter_bots/ocaml/MyBot.native" \
+"absolute-path-to/aichallenge/wargame/dist/starter_bots/ocaml/MyBot.native"
