173826cdce8c6b4b968f40e8c4c45ea695cac12f
Jude Hungerford <edward.h@gmx.net>
2012-03-17 15:20:14 +1100

    basic visualizer

--- wargame/example-game.sh
+++ wargame/example-game.sh
@@ -1,3 +1,3 @@
-python2 playgame.py -v --nolaunch -l game_logs -I -O -E --turns 100 --map_file maps/test/war_p02_01.map \
+python2 playgame.py -v -l game_logs -I -O -E --turns 100 --map_file maps/test/war_p02_01.map \
 "absolute-path-to/aichallenge/wargame/dist/starter_bots/ocaml/MyBot.native" \
 "absolute-path-to/aichallenge/wargame/dist/starter_bots/ocaml/MyBot.native"
