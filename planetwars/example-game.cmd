06f8d49353c27c760a29c0e1266ae0950d9c880c
UncleVasya (Oleg Ovharenko) <Oleg_Ovcharenko@list.ru>
2012-11-18 18:21:07 +0200

    Removed Tron stuff, added PW starterpacks.

--- planetwars/example-game.cmd
+++ planetwars/example-game.cmd
@@ -1,2 +1,2 @@
-python playgame.py --verbose --fill --log_input --log_output --log_error --nolaunch --log_dir game_logs --turns 500 --map_file maps/planetwars_01.map PW_starter_pack.exe PW_starter_pack.exe
+python playgame.py --verbose --fill --log_input --log_output --log_error --log_dir game_logs --turns 500 --map_file maps/planetwars_05.map "python dist/starter_bots/python/MyBot.py" "python dist/starter_bots/python/MyBot.py"
 pause
